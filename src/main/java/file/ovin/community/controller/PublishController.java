75a1f5aefd52808ab1252fb4b12158952e7ecc3f
Ovin-130 <2240738428@qq.com>
2020-04-11 13:01:51 +0800

    add publish.html

--- src/main/java/file/ovin/community/controller/PublishController.java
+++ src/main/java/file/ovin/community/controller/PublishController.java
@@ -1,4 +1,14 @@
 package file.ovin.community.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class PublishController {
+
+    @GetMapping("/publish")
+    public String publish(){
+        System.out.println("==============================");
+        return "publish";
+    }
 }
